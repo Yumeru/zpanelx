b6cc8702ec9d73f3c5d211bdcd4ed8557291982b
Kenneth Chow <kenneth.k.t.chow@gmail.com>
2012-09-07 03:08:12 -0700

    Fixed my tab spacing inconsistencies

--- dryden/cache/data.class.php
+++ dryden/cache/data.class.php
@@ -17,80 +17,80 @@ class cache_data {
      * stores the cache data for the user session (persist over shadowing)
      * @var array
      */
-	private $cacheData = array();
-	
+    private $cacheData = array();
+    
     /**
      * stores the current owner id of the information being worked on
      * @var string
      */
-	private $userId = null;
+    private $userId = null;
  
- 	/**
+    /**
      * Stores data in cache
      * @author Kenneth Chow
      * @param int $key The key to pair up with the value for caching.
-	 * @param mixed $value The value to store in cache.
-	 * @param int|bool|null $expire Number of seconds before this value in cache expires. Use true for default time, false for no expiry, or null to keep existing expiry time (if rewriting value)
+     * @param mixed $value The value to store in cache.
+     * @param int|bool|null $expire Number of seconds before this value in cache expires. Use true for default time, false for no expiry, or null to keep existing expiry time (if rewriting value)
      * @return void
      */
-	public function cacheData($key, $value = '', $expire = true) {
-		$cache = &pointToUserIdDataSet();
-		
-		if (!isset($cache[$key]) || $cache[$key]->hasExpired())
-			$cache[$key] = new cache_valuepair($value, $expire);
-			//TODO - add exception case here for unexpected class type
-		else
-			$cache[$key]->set($value, $expire);
-	}
-	
- 	/**
+    public function cacheData($key, $value = '', $expire = true) {
+        $cache = &pointToUserIdDataSet();
+        
+        if (!isset($cache[$key]) || $cache[$key]->hasExpired())
+            $cache[$key] = new cache_valuepair($value, $expire);
+            //TODO - add exception case here for unexpected class type
+        else
+            $cache[$key]->set($value, $expire);
+    }
+    
+    /**
      * Retrieve data in cache
      * @author Kenneth Chow
      * @param int $key The key that is paired up with the value in cache
-	 * @param bool $ignoryExpiry Retrieve the value even if it has expired already
+     * @param bool $ignoryExpiry Retrieve the value even if it has expired already
      * @return mixed
      */
-	public function retrieveData($key, $ignoreExpiry = false) {
-		$cache = &pointToUserIdDataSet();
-		
-		if (!isset($cache[$key]))
-			return null;
-		
-		if ($ignoreExpiry)
-			return $cache[$key]->getEvenIfExpired();
-		else
-			return $cache[$key]->get();
-	}
-	
- 	/**
+    public function retrieveData($key, $ignoreExpiry = false) {
+        $cache = &pointToUserIdDataSet();
+        
+        if (!isset($cache[$key]))
+            return null;
+        
+        if ($ignoreExpiry)
+            return $cache[$key]->getEvenIfExpired();
+        else
+            return $cache[$key]->get();
+    }
+    
+    /**
      * Sets the account id pointer of the cache for all data manipulation
      * @author Kenneth Chow
      * @param int $userId The account id which the cache should match the data set with
      * @return void
      */
-	public function setCacheUserId($userId) {
-		$this->userId = $userId;
-	}
-		
-	/**
+    public function setCacheUserId($userId) {
+        $this->userId = $userId;
+    }
+        
+    /**
      * Returns a reference to the dataset belonging to the current user
      * @author Kenneth Chow
      * @param int $zpuid The ZPanel user account ID to set the session as.
      * @return void
      */
-	private function &pointToUserIdDataSet() {
-		if (!isset($this->userId))
-			$this->userId = ctrl_auth::CurrentUserID();
-			
-		$userId = $this->userId;
-	
-		if (!isset($this->cacheData[$userId]))
-			$this->cacheData[$userId] = array();
-		
-		return $this->cacheData[$userId];
-	}
+    private function &pointToUserIdDataSet() {
+        if (!isset($this->userId))
+            $this->userId = ctrl_auth::CurrentUserID();
+            
+        $userId = $this->userId;
+    
+        if (!isset($this->cacheData[$userId]))
+            $this->cacheData[$userId] = array();
+        
+        return $this->cacheData[$userId];
+    }
 }
 
 ?>
-		
-		
+        
+        
