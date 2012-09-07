2a80b2b8831f07c13b3a595db7c42dfc6219d1bf
Kenneth Chow <kenneth.k.t.chow@gmail.com>
2012-09-07 02:56:59 -0700

    Added classes to cache values that are frequently used
    Only the classes are added, they are not currently used until the other
    classes are modified to take advantage of these classes.
    
    Code are not tested until I can get the LAMP virtual box to work with
    zpanelx.

---
+++ dryden/cache/data.class.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * Performance optimizer - this class simply cache all the user/account details to reduce the number of database lookups
+ * @package zpanelx
+ * @subpackage dryden -> cache
+ * @version 1.0.0
+ * @author Kenneth Chow
+ * @copyright ZPanel Project (http://www.zpanelcp.com/)
+ * @link http://www.zpanelcp.com/
+ * @license GPL (http://www.gnu.org/licenses/gpl.html)
+ * @todo adding the code needed to set this cache engine to work
+ */
+class cache_data {
+
+    /**
+     * stores the cache data for the user session (persist over shadowing)
+     * @var array
+     */
+	private $cacheData = array();
+	
+    /**
+     * stores the current owner id of the information being worked on
+     * @var string
+     */
+	private $userId = null;
+ 
+ 	/**
+     * Stores data in cache
+     * @author Kenneth Chow
+     * @param int $key The key to pair up with the value for caching.
+	 * @param mixed $value The value to store in cache.
+	 * @param int|bool|null $expire Number of seconds before this value in cache expires. Use true for default time, false for no expiry, or null to keep existing expiry time (if rewriting value)
+     * @return void
+     */
+	public function cacheData($key, $value = '', $expire = true) {
+		$cache = &pointToUserIdDataSet();
+		
+		if (!isset($cache[$key]) || $cache[$key]->hasExpired())
+			$cache[$key] = new cache_valuepair($value, $expire);
+			//TODO - add exception case here for unexpected class type
+		else
+			$cache[$key]->set($value, $expire);
+	}
+	
+ 	/**
+     * Retrieve data in cache
+     * @author Kenneth Chow
+     * @param int $key The key that is paired up with the value in cache
+	 * @param bool $ignoryExpiry Retrieve the value even if it has expired already
+     * @return mixed
+     */
+	public function retrieveData($key, $ignoreExpiry = false) {
+		$cache = &pointToUserIdDataSet();
+		
+		if (!isset($cache[$key]))
+			return null;
+		
+		if ($ignoreExpiry)
+			return $cache[$key]->getEvenIfExpired();
+		else
+			return $cache[$key]->get();
+	}
+	
+ 	/**
+     * Sets the account id pointer of the cache for all data manipulation
+     * @author Kenneth Chow
+     * @param int $userId The account id which the cache should match the data set with
+     * @return void
+     */
+	public function setCacheUserId($userId) {
+		$this->userId = $userId;
+	}
+		
+	/**
+     * Returns a reference to the dataset belonging to the current user
+     * @author Kenneth Chow
+     * @param int $zpuid The ZPanel user account ID to set the session as.
+     * @return void
+     */
+	private function &pointToUserIdDataSet() {
+		if (!isset($this->userId))
+			$this->userId = ctrl_auth::CurrentUserID();
+			
+		$userId = $this->userId;
+	
+		if (!isset($this->cacheData[$userId]))
+			$this->cacheData[$userId] = array();
+		
+		return $this->cacheData[$userId];
+	}
+}
+
+?>
+		
+		
---
+++ dryden/cache/valuepair.class.php
@@ -0,0 +1,120 @@
+<?php
+
+/**
+ * Simple object to store a timestamped value for the cache, object will provide no values if it expires
+ * @package zpanelx
+ * @subpackage dryden -> cache
+ * @version 1.0.0
+ * @author Kenneth Chow
+ * @copyright ZPanel Project (http://www.zpanelcp.com/)
+ * @link http://www.zpanelcp.com/
+ * @license GPL (http://www.gnu.org/licenses/gpl.html)
+ */
+class final cache_valuepair {
+
+    /**
+     * Constant in seconds to store default expiry time, default is 5 minutes
+     * @var int
+     */
+	const DEFAULT_EXPIRE_TIME = 300;
+
+	/**
+     * Cached value for retrieval later, can be anything
+     * @var mixed
+     */
+	private $value;
+	
+	/**
+	 * Number of seconds to expire the value
+	 * @var int
+	 */
+	private $expire;
+	
+	/**
+	 * Time when this value was created/updated
+	 * @var int
+	 */
+	private $timestamp;
+	
+	/**
+	 * Sets the flag whether this value should expire eventually or not
+     * @var bool
+     */	 
+	private $no_expiry = false;
+	
+ 	/**
+     * Default constructor for the valuepair
+     * @author Kenneth Chow
+     * @param int $value The value to store for caching.
+	 * @param mixed $expire Optional, expire this cache after number of seconds, use default if true/null, or no expiry for false
+     */
+	public function __construct($value, $expire = true)
+	{	
+		// at this time, there are no existing value so case <true> is the same as case <null>
+		if (!$isset($expire))
+			$expire = true;
+	
+		$this->set($value, $expire);
+	}
+	
+ 	/**
+     * Retrieves value if it still has not expired
+     * @author Kenneth Chow
+	 * @return mixed The value to retrieve. If it has expired then null will be returned instead.
+     */
+	public function get()
+	{
+		if (!$this->hasExpired())
+			return $value;
+		else
+			return null;
+	}
+	
+ 	/**
+     * Retrieves value regardless of expiry
+     * @author Kenneth Chow
+	 * @return mixed The value to retrieve.
+     */
+	public function getEvenIfExpired()
+	{
+		return $value;
+	}
+	
+ 	/**
+     * Updates the value, and give it a new timestamp, and new expiry time span if desired
+     * @author Kenneth Chow
+     * @param int $value The value to store for caching.
+	 * @param mixed $expire Optional, expire this cache after number of seconds, use default if true, keep existing expiry amount if null, or no expiry for false
+     */
+	public function set($value = '', $expire)
+	{
+		$this->value = $value;
+		
+		if ($expire === false)
+			$this->no_expiry = true;
+		
+		if ($expire === true)
+			$expire = self::DEFAULT_EXPIRE_TIME;
+
+		$this->timestamp = time();
+		
+		if ($expire !== null)
+			$this->expire = $expire;
+	}
+	
+
+ 	/**
+     * Checks if the value has expired already
+     * @author Kenneth Chow
+	 * @param bool If expired then true will be returned, otherwise false will be returned instead
+     */
+	private function hasExpired()
+	{
+		if (time() < $this->timestamp + $this->expire && !$this->no_expiry)
+			return true;
+		else
+			return false;
+	}
+}
+
+?>
